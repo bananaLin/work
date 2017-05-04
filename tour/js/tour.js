589c80419a94c72f5a48cac1889e9253a384e3a4
junior <527929821@qq.com>
2017-05-04 10:35:17 +0800

    change

--- tour/js/tour.js
+++ tour/js/tour.js
@@ -7,10 +7,15 @@ $(function(){
            $('.phones').css('display', 'none');
      });
 	 
+	 
+	 $(document).on('click', '.labor-day', function(){
+           $('.labor-day').css('display', 'none');
+     });
 
      window.onscroll = function(){
+
      	var top = document.body.scrollTop;
-     	document.getElementsByClassName("labor-day")[0].style.top = top + 484;
+  
      	document.getElementsByClassName("scroll-div")[0].style.top = top + 424;
      
      	if(top == 0){
